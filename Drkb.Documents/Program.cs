067bc910a2acc601ad60b4c5e187aac926dd0322
Vadim <gaskov.vadim@mail.ru>
2026-04-27 14:14:18 +0800

    added logger

--- Drkb.Documents/Program.cs
+++ Drkb.Documents/Program.cs
@@ -5,63 +5,74 @@ using Drkb.Documents.Domain.Enum;
 using Drkb.Documents.Infrastructure.Data.HistoryWriter;
 using Drkb.JwtConfiguration.DI;
 using Drkb.Documents.Infrastructure.DI;
-using Drkb.Documents.Infrastructure.LoggerConfiguration;
 using Drkb.Documents.Infrastructure.Services.Authorization;
+using Drkb.Logger.Serilog;
 using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+try
+{
+    Log.Information("Starting application");
+    
+    var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddCorsPolicies();
-builder.Services.AddDbContext(builder.Configuration);
-builder.Services.AddJwtAuthentication(builder.Configuration);
-builder.Services.AddJwtGeneration();
-builder.Services.AddBehavior();
-builder.Services.AddMediatr();
-builder.Services.AddDataProviderServices();
-builder.Services.AddSwagger();
+    builder.Host.UseDrkbSerilog(builder.Configuration, "Documents"); //Второй аргумент Application 
+    
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddHttpContextAccessor();
+    builder.Services.AddCorsPolicies();
+    builder.Services.AddDbContext(builder.Configuration);
+    builder.Services.AddJwtAuthentication(builder.Configuration);
+    builder.Services.AddJwtGeneration();
+    builder.Services.AddBehavior();
+    builder.Services.AddMediatr();
+    builder.Services.AddDataProviderServices();
+    builder.Services.AddSwagger();
 
-builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
-builder.Services.AddScoped<IHistoryWriter<Document, DocumentHistoryChangeType>, DocumentHistoryWriter>();
+    builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
+    builder.Services.AddScoped<IHistoryWriter<Document, DocumentHistoryChangeType>, DocumentHistoryWriter>();
 
-builder.Services.AddSerilogLogger();
-Log.Logger = SerilogConfiguration.GetSerilogConfiguration(builder.Configuration);
-builder.Host.UseSerilog();
-
-var app = builder.Build();
+    var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+    else
+    {
+        app.UseSwagger()
+            .UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "DrkbNews");
+                options.RoutePrefix = string.Empty;
+            });
+    }
+
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseCors("AllowFrontend");
+    app.UseRouting();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
 }
-else
+catch (Exception ex)
 {
-    app.UseSwagger()
-        .UseSwaggerUI(options =>
-        {
-            options.SwaggerEndpoint("/swagger/v1/swagger.json", "DrkbNews");
-            options.RoutePrefix = string.Empty;
-        });
+    Log.Fatal(ex, "Application terminated unexpectedly");
 }
-
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseCors("AllowFrontend");
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+finally
+{
+    Log.CloseAndFlush();
+}
